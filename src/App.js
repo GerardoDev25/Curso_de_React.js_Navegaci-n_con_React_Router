b4b9e8ba201b5d145d75c94e8f2cdba8faa1b9a1
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-05 09:58:03 -0400

    [200~Route: componentes de navegación q
    !
    ]

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+import { Menu } from './Menu.js';
+import { HomePage } from './HomePage.js';
+import { BlogPage } from './BlogPage.js';
+import { ProfilePage } from './ProfilePage.js';
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<p>not Found</p>} />
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
