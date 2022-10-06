cc3b38e0d6ecec464e4a9522e45f8435957a5c3c
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-05 22:07:50 -0400

    Menú con rutas públicas y privadas

--- src/Menu.js
+++ src/Menu.js
@@ -1,46 +1,59 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from './auth';
 
 const routes = [];
 routes.push(
   {
     to: '/',
     text: 'Home',
+    private: false,
   },
   {
     to: '/blog',
     text: 'Blog',
+    private: false,
   },
   {
     to: '/profile',
     text: 'Profile',
+    private: true,
   },
   {
     to: '/login',
     text: 'Login',
+    publicOnly: true,
+    private: false,
   },
   {
     to: '/logout',
     text: 'Logout',
+    private: true,
   }
 );
 
 export const Menu = () => {
+  const auth = useAuth();
+
   return (
     <nav>
       <ul>
-        {routes.map(({ to, text }, index) => (
-          <li key={index}>
-            <NavLink
-              to={to}
-              style={({ isActive }) => ({
-                color: isActive ? 'red' : 'blue',
-              })}
-            >
-              {text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map(({ to, text, private: isPrivate, publicOnly }, index) => {
+          if (publicOnly && auth.user) return null;
+          if (isPrivate && !auth.user) return null;
+          return (
+            <li key={index}>
+              <NavLink
+                to={to}
+                style={({ isActive }) => ({
+                  color: isActive ? 'red' : 'blue',
+                })}
+              >
+                {text}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
